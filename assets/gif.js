628f4b9ddc820fd9e0395589e20342228495803d
Rey Cruz <reycruz@Reys-MacBook-Pro.local>
2017-10-29 20:59:20 -0700

    button for search bar works and new movies are display on the page when user types it in.

---
+++ assets/gif.js
@@ -0,0 +1,73 @@
+// apiKey
+// wpcti61gVVsZVscWNonebhBb7euepWEN
+
+
+// Initial array of movies
+var movies = ["Deadpool", "Moana", "Fantastic Beasts", "Get Out"];
+
+
+
+// displayMovieInfo function re-renders the HTML to display the appropriate content
+      function displayMovieInfo() {
+        var movie = $("#movie-input").val();
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        movie + "&api_key=wpcti61gVVsZVscWNonebhBb7euepWEN&limit=10";
+        // Creating an AJAX call for the specific movie button being clicked
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+        	console.log(response);
+      
+      	//code to get GIFS go here
+
+        });
+      }
+
+
+
+
+
+
+
+
+
+// Function for displaying movie data
+function renderButtons() {
+
+    // Delete the content inside the allMovies div prior to adding new movies
+    // (this is necessary otherwise you will have repeat buttons)
+
+    // this will loop through the movies in the array and it will generate invividual buttons for each movie in the array loop
+
+    $("#allMovies").empty();
+    for (var i = 0; i < movies.length; i++) {
+        var btn = $("<button>");
+        btn.text(movies[i]);
+        $("#allMovies").append(btn)
+
+    }
+
+}
+
+// when search is clicked this function runs 
+$("#addMovie").on("click", function(event) {
+
+    event.preventDefault();
+
+    //users movie search appers on the screen 
+    var movieTitle = $("#movie-input").val();
+    movies.push(movieTitle);
+
+
+    // function to render the list of movie buttons
+    renderButtons();
+});
+
+  // Adding a click event listener to all elements with a class of "movie"
+      $(document).on("click", ".movie", displayMovieInfo);
+
+// the initial list of movies will show upon opening the web app 
+renderButtons();
+
+
