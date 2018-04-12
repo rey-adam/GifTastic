231965e00d0b51d852f7436c0bd4c1dd79b59cb7
Rey Cruz <reycruz@Reys-MBP.attlocal.net>
2018-04-11 21:46:17 -0700

    gifs can be pause and play, trying to make the search button work

--- assets/gif.js
+++ assets/gif.js
@@ -2,21 +2,21 @@
 // wpcti61gVVsZVscWNonebhBb7euepWEN
 
 $(function(){
-    populateButtons(movies, 'movieButton', '#allMovies')
+    populateButtons(moviesArray, 'movieButton', '#allMovies')
     console.log("page loaded")
 });
 
-var movies = ["Deadpool", "Moana", "Fantastic Beasts", "Get Out", "Titanic", "Iron Man"];
+var moviesArray = ["Deadpool", "Moana", "Fantastic Beasts", "Get Out", "Titanic", "Iron Man"];
 
   // function to make buttons and add to page
-  function populateButtons(movies, classToAdd, areaToAddTo) {
+  function populateButtons(moviesArray, classToAdd, areaToAddTo) {
     $(areaToAddTo).empty();
 
-    for (var i = 0; i < movies.length; i++) {
+    for (var i = 0; i < moviesArray.length; i++) {
         var a = $("<button>");
         a.addClass(classToAdd);
-        a.attr("data-type", movies[i]);
-        a.text(movies[i]);
+        a.attr("data-type", moviesArray[i]);
+        a.text(moviesArray[i]);
         $(areaToAddTo).append(a);
       }
     }
@@ -47,79 +47,22 @@ $(document).on('click','.movieButton',function(){
   })
 })
 
+// onclick gifs to make it still and animate 
+$(document).on('click', '.searchMovie', function(){
+  var state = $(this).attr('data-state');
+  if (state === 'still'){
+    $(this).attr('src', $(this).data('animated'));
+    $(this).attr('data-state', 'animated');
+  } else {
+    $(this).attr('src', $(this).data('still'));
+    $(this).attr('data-state', 'still');
+  }
+})
 
-
-
-
-// // Initial array of movies
-// var movies = ["Deadpool", "Moana", "Fantastic Beasts", "Get Out"];
-
-
-
-// // displayMovieInfo function re-renders the HTML to display the appropriate content
-//       function displayMovieGifs() {
-//         var movie = $("#movie-input").val();
-//         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-//         movie + "&api_key=wpcti61gVVsZVscWNonebhBb7euepWEN&limit=10";
-//         // var queryURL = "https://api.giphy.com/v1/gifs/search?q=titanic&api_key=wpcti61gVVsZVscWNonebhBb7euepWEN&limit=10";
-//         // Creating an AJAX call for the specific movie button being clicked
-//         $.ajax({
-//           url: queryURL,
-//           method: "GET"
-//         }).done(function(response) {
-//         	console.log(response);
-      
-//       	//code to get GIFS go here to show on the page 
-
-//         });
-//       }
-
-
-// //MISSING 
-// // TO MAKE THE GIFS PAUSE AND PLAY 
-
-
-
-
-
-
-
-// // Function for displaying movie data
-// function renderButtons() {
-
-//     // Delete the content inside the allMovies div prior to adding new movies
-//     // (this is necessary otherwise you will have repeat buttons)
-
-//     // this will loop through the movies in the array and it will generate invividual buttons for each movie in the array loop
-
-//     $("#allMovies").empty();
-//     for (var i = 0; i < movies.length; i++) {
-//         var btn = $("<button>");
-//         btn.text(movies[i]);
-//         $("#allMovies").append(btn)
-
-//     }
-
-// }
-
-// // when search is clicked this function runs 
-// $("#addMovie").on("click", function(event) {
-
-//     event.preventDefault();
-
-//     //users movie search appears on the screen 
-//     var movieTitle = $("#movie-input").val();
-//     movies.push(movieTitle);
-
-
-//     // function to render the list of movie buttons
-//     renderButtons();
-// });
-
-//   // Adding a click event listener to all elements with a class of "movie"
-//       $(document).on("click", displayMovieGifs);
-
-// // the initial list of movies will show upon opening the web app 
-// renderButtons();
-
-
+// adding a new movie to the array/list after searching for it
+$('#addSearch').on('click', function(){
+  var newSearch = $('input').eq(0).val();
+  moviesArray.push(newSearch);
+  populateButtons(moviesArray, 'movieButton', '#allMovies');
+  return false;
+})
